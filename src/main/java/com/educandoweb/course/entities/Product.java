9a1e85152d961903f0cf5b9551780efb1a651d4f
Lucas Sena <lucasferreiracyrax@gmail.com>
2025-12-03 18:45:24 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -0,0 +1,89 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "tb_products")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String descripition;
+	private Double price;
+	private String imgUrl;
+	
+	@Transient
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+	}
+
+	public Product(Long id, String name, String descripition, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descripition = descripition;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescripition() {
+		return descripition;
+	}
+
+	public void setDescripition(String descripition) {
+		this.descripition = descripition;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	
+	
+	
+}
