36797d2ac5d132c1b0b6f19269fd3be197eaf0ee
Lucas Sena <lucasferreiracyrax@gmail.com>
2025-12-08 17:31:45 -0300

    OrderItem, ManyToMany association with extra attributes PART 1

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
